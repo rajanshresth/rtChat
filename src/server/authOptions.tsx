b7fa43554230222434eb05c70b4a18d9a5ad2a22
rajanshresth <shrestharajan726@gmail.com>
2024-01-24 22:22:56 +0545

    nextauth removed

--- src/server/authOptions.tsx
+++ src/server/authOptions.tsx
@@ -1,5 +1,5 @@
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import prisma from "./db/client";
 
